839b1a3aeb0be2a94be9c1804d0315c92909a90e
Mariana Marques <mrqsp.rg@gmail.com>
2022-11-23 21:54:24 -0300

    reinitialized files

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mariana Marques</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script href="js/script.js"></script>
+</head>
+<body>
+    <div class="header">
+    <a href="index2.php">
+        <img class="logo" src="img/logo.png">
+    </a>
+    </div>
+
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">   
+                <div class="modulo azul">
+                    <h3><strong>PROGRAMAÇÃO WEB</strong></h3>
+                    <ul>
+                                            
+                        <li>
+                            <a href="index\cafeteria\index.html">
+                                Front-end: Cafeteria
+                            </a>
+                        </li>
+                     
+                        <li>
+                            <a href="index\calculadora.php">
+                                Operações matemáticas 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="index\conversor.php">
+                                Conversor de Sistemas Númericos
+                            </a>
+                        </li>
+
+                        <!-- 
+                        <li>
+                            <a href="index\cafeteria\index.html">
+                                Formulário
+                            </a>
+                        </li>
+                        -->
+                        <li>
+                            <a href="file:///C:/Users/Lenovo/Desktop/Ciência%20da%20Computação/1%20Semestre/Programação%20web/Avaliação%201/pweb_MarianaMarques/provaWeb_MarianaMarques/indexProva1.html">
+                                Avaliação 1 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="index\calculadoras\js_calculadora\index.html">
+                                Calculadora 
+                            </a>
+                        </li>
+                        <!-- 
+                        <li>
+                            <a href="index\js_calculadoraFuncional\index.html">
+                                Calculadora Js Funcional (porém feia)
+                            </a>
+                        </li>
+                        -->
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                  <h3><strong>LTPI</strong></h3>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/myoui01/projetosJava">
+                                Lista de exercícios 1  
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/myoui01/projetosJava">
+                                Lista de exercícios 2 
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3><strong>INICIAÇÃO A STARTUPS</strong></h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">  
+                            Trabalho sobre Localiza              
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">  
+                            Ideação Reppub!              
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">  
+                            Brainstorming          
+                            </a>
+                        </li>
+                    </ul>
+                </div> 
+                <br>
+                <div>
+                    <ul>
+                    <ul>
+                </div>
+            </nav>
+        </div>
+    </main>       
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mariana Marques</title>
+    <link rel="stylesheet" href="css/style3.css">
+    <script href="js/script.js"></script>
+</head>
+<body>
+    <div class="header">
+    <a class="logo"  href="index.php"><img src="img/logotek.png" height="250px"></a>
+    </div>
+
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">   
+                <div class="modulo azul">
+                    <h3><strong>PHP</strong></h3>
+                    <ul>
+                        <!--
+                        <li>
+                            <a href="index\calculadora.php">
+                                Calculadora PHP Formulário
+                            </a>
+                        </li>
+                     
+                        <li>
+                            <a href="index\conversor.php">
+                                Conversor de Sistemas Númericos
+                            </a>
+                        </li>
+                        -->
+            
+                        <li>
+                            <a href="index\cafeteria\index.html">
+                                Projeto Final - Innout
+                            </a>
+                        </li>   
+
+                        <!-- 
+                        <li>
+                            <a href="index\cafeteria\index.html">
+                                Formulário
+                            </a>
+                        </li>
+                        -->
+                        <li>
+                            <a href="index\calculadoras\js_calculadora\index.html">
+                                Organizador de Tarefas
+                            </a>
+                        </li>
+
+      
+                        <!-- 
+                        <li>
+                            <a href="index\js_calculadoraFuncional\index.html">
+                                Calculadora Js Funcional (porém feia)
+                            </a>
+                        </li>
+                        -->
+                    </ul>
+                </div>
+                <div class="modulo verde">
+                  <h3><strong>JAVASCRIPT</strong></h3>
+                    <ul>
+                        <li>
+                            <a href="index\calculadoras\js_calculadora\index.html">
+                                Flippy Bird
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo amarelo">
+                    <h3><strong>DOCUMENTOS</strong></h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                              Logins e senhas
+                            </a>
+                        </li>
+                  
+    
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                              Links importantes
+                            </a>
+                        </li>
+                      
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                              Resumos
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div> 
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        TEKNISA © <?= date('Y'); ?>
+    </footer>
+
+       
+</body>
+</html>
